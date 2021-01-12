b2c6b0868b4e69dc7e45b4e1c5604eac2d9f2c1c
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-12 08:13:59 +0000

    babel-loader added to webpack for JS transpilation, alongside the @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     filename: './dist',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+    ],
+  },
 };
