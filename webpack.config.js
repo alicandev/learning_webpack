2f165b31a539a12770bac1c1bbe614093cb2c9d2
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-13 05:04:21 +0000

    uglifyjs-webpack-plugin used to minize code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: {
-    about: './src/about.js',
-    contact: './src/contact.js',
-  },
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
+    minimizer: [new UglifyJsPlugin()],
     splitChunks: {
       chunks: 'all',
     },
   },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
@@ -26,8 +29,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
       },
       {
